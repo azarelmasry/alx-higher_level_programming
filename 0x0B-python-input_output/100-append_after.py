d309098ea18153ba74b8eacb34a8a8e1bebbe1f6
azarelmasry <azarelmasry@gmail.com>
2023-11-07 06:36:45 -0800

    0p.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""append after function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appends new_string after a line containing in file_name"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)
