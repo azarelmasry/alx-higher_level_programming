c309e8d1bda370ccabb707966e10a7ba187484a0
azarelmasry <azarelmasry@gmail.com>
2023-11-10 06:37:09 -0800

    0c.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """Internal method to updates instance attributes"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
