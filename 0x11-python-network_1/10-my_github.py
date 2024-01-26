70928a1be7776cbd95467116a21c74884eb0e3cf
azarelmasry <azarelmasry@gmail.com>
2024-01-26 05:19:38 -0800

    11.pet

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""takes your GitHub credentials |(username and password)|
+and uses the GibHub API to displya id"""
+if __name__ == "__main__":
+    import sys
+    import requests
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    print(r.json().get('id'))
